da9ffb3d168a160783f69ff62657ee4fe2180ce8
Emre Kaygılı <tekiemre6@gmail.com>
2025-04-29 16:47:20 +0300

    .

---
+++ src/main/java/com/ai/springAiGenerate/SpringAiGenerateApplication.java
@@ -0,0 +1,14 @@
+package com.ai.springAiGenerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringAiGenerateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiGenerateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/springAiGenerate/SpringAiGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.springAiGenerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiGenerateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
